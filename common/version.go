acf3f47ec392d04b435a1ef057a2c77f0ff41f0a
kai <kai@DESKTOP-R5AE957>
2025-03-24 23:24:03 +0900

    微修正

--- common/version.go
+++ common/version.go
@@ -1,14 +1,12 @@
 package common
 
 import (
-	"log"
 	"runtime/debug"
 )
 
 func GetVersionFromGit() string {
 	bi, ok := debug.ReadBuildInfo()
 	if ok {
-		log.Printf(bi.Main.Version)
 		return bi.Main.Version
 	}
 	return "Unknown"
